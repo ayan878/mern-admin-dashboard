2470fcf1bfb3c3d88d6dfa356e76beb909142d3b
ayan878 <ayanraza197@gmail.com>
2024-07-16 22:25:24 +0530

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import {
   AppBar,
+  Box,
+  Button,
   IconButton,
   InputBase,
   Toolbar,
+  Typography,
   useTheme,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import FlexBetween from "./FlexBetween";
 import {
@@ -14,15 +17,25 @@ import {
   DarkModeOutlined,
   LightModeOutlined,
   Settings,
+  ArrowDropDownOutlined,
 } from "@mui/icons-material";
 import { setMode } from "state";
+import profileImage from "assets/ayan.jpg";
 
-function Navbar({ isSidebarOpen, setIsSidebarOpen }) {
+
+function Navbar({user, isSidebarOpen, setIsSidebarOpen }) {
   const dispatch = useDispatch();
   const theme = useTheme();
 
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isOpen = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => setAnchorEl(null);
+
   return (
-    <AppBar sx={{ position: "static", background: "none", boxShadow: "none"}}>
+    <AppBar sx={{ position: "static", background: "none", boxShadow: "none" }}>
       <Toolbar sx={{ justifyContent: "space-between" }}>
         {/* LEFT SIDE */}
         <FlexBetween>
@@ -53,6 +66,44 @@ function Navbar({ isSidebarOpen, setIsSidebarOpen }) {
           <IconButton>
             <Settings />
           </IconButton>
+        </FlexBetween>
+        <FlexBetween>
+          <Button
+            onClick={handleClick}
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              textTransform: "none",
+              gap: "1rem",
+            }}
+          >
+            <Box
+              component="img"
+              alt="profile"
+              src={profileImage}
+              height="32px"
+              width="32px"
+              borderRadius="50%"
+              sx={{ objectFit: "cover" }}
+            />
+            <Box textAlign="left">
+              <Typography
+                fontWeight="bold"
+                fontSize="0.85rem"
+                sx={{ color: theme.palette.secondary[100] }}
+              >
+                {user.name}
+              </Typography>
+              <Typography
+                fontSize="0.75rem"
+                sx={{ color: theme.palette.secondary[200] }}
+              >
+                {user.occupation}
+              </Typography>
+              <ArrowDropDownOutlined sx={{}}/>
+            </Box>
+          </Button>
         </FlexBetween>
       </Toolbar>
     </AppBar>
