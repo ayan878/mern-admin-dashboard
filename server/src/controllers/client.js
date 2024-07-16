0398b593484fc2527794f9d2fb086ebafce0bd6b
ayan878 <ayanraza197@gmail.com>
2024-07-17 04:27:51 +0530

    controller

--- server/src/controllers/client.js
+++ server/src/controllers/client.js
@@ -1,4 +1,17 @@
-import express from 'express'
+import Product from "../models/Product";
+import ProductStat from "../models/ProductStat";
 
-const router=express.Router();
-export default router;
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    const productsWithStats = await Promise.all(
+      products.map(async (product) => {
+        const stat = await ProductStat.find({ productId: product._id });
+        return { ...product._doc, stat };
+      })
+    );
+    res.status(200).json(productsWithStats);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
