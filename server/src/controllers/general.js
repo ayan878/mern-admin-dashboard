57732c62a718cbd005c18f746c6d5485d170fb59
ayan878 <ayanraza197@gmail.com>
2024-07-16 22:25:14 +0530

    general

--- server/src/controllers/general.js
+++ server/src/controllers/general.js
@@ -3,9 +3,12 @@ import User from "../models/User.js";
 export const getUser = async (req, res) => {
   try {
     const { id } = req.params;
-    const user = await User.findById({ id });
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
     res.status(200).json(user);
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
