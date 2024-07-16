e785ae00a6e47f5407fa6f07a2d48e3299aab193
ayan878 <ayanraza197@gmail.com>
2024-07-16 22:25:01 +0530

    index

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.js'
-import './index.css'
-import { configureStore } from '@reduxjs/toolkit'
-import globalReducer from "./state/index.js"
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.js";
+import "./index.css";
+import { configureStore } from "@reduxjs/toolkit";
+import globalReducer from "./state/index.js";
+import { Provider } from "react-redux";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { api } from "./state/api.js";
 
-const store=configureStore({
-  reducer:{
-    global:globalReducer,
-  }
-})
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
+});
+
+setupListeners(store.dispatch);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
