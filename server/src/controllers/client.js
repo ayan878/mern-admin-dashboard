cf3bd0597f3bc51856b1fa2ef23a714e568caca9
ayan878 <ayanraza197@gmail.com>
2024-07-18 16:05:23 +0530

    controllers

--- server/src/controllers/client.js
+++ server/src/controllers/client.js
@@ -1,5 +1,7 @@
-import Product from "../models/Product";
-import ProductStat from "../models/ProductStat";
+import Product from "../models/Product.js";
+import ProductStat from "../models/ProductStat.js";
+import Transaction from "../models/Transactions.js";
+import User from "../models/User.js";
 
 export const getProducts = async (req, res) => {
   try {
@@ -11,6 +13,50 @@ export const getProducts = async (req, res) => {
       })
     );
     res.status(200).json(productsWithStats);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getCustomers = async (req, res) => {
+  try {
+    const customers = await User.find({ role: "user" }).select("-password");
+    res.status(200).json(customers);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getTransactions = async (req, res) => {
+  try {
+    // sort should look like this {"field":"userId","sort":"desc"}
+    const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;
+
+    // formatted sort should look like {"userId": -1}
+    const generateSort = () => {
+      const sortParse = JSON.parse(sort);
+      return {
+        [sortParse.field]: sortParse.sort === "asc" ? 1 : -1,
+      };
+    };
+
+    const sortFormatted = sort ? generateSort() : {};
+
+    const transactions = await Transaction.find({
+      $or: [
+        { cost: { $regex: new RegExp(search, "i") } },
+        { userId: { $regex: new RegExp(search, "i") } },
+      ],
+    })
+      .sort(sortFormatted)
+      .skip((page - 1) * pageSize)
+      .limit(pageSize);
+
+    const total = await Transaction.countDocuments({
+      name: { $regex: search, $options: "i" },
+    });
+
+    res.status(200).json({ transactions, total });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
