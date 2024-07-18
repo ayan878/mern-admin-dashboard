170aa7f8acb021570b8aab91c6bbf6835298992b
ayan878 <ayanraza197@gmail.com>
2024-07-18 16:04:06 +0530

    app

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,10 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
 import Layout from "scenes/layout";
 import Dashboard from "scenes/dashboard"; 
+import Products from "scenes/products";
+import Customers from "scenes/customers";
+import Transactions from "scenes/transactions";
+
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -18,8 +22,11 @@ function App() {
           <CssBaseline />
           <Routes>
             <Route element={<Layout />}>
-            <Route path="/" element={<Navigate to="/dashboard" replace />} />
-            <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/customers" element={<Customers />} />
+              <Route path="/transactions" element={<Transactions />} />
             </Route>
           </Routes>
         </ThemeProvider>
